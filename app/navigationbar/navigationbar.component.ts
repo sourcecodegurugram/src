8f93a572de8616afaa6855acebba526105bba3d0
Gagan Sawhney <gagansawhney@gmail.com>
2020-05-28 13:04:04 +0530

    Fixed it so the CSRF and X-cookie work and auth works with xhr

--- app/navigationbar/navigationbar.component.ts
+++ app/navigationbar/navigationbar.component.ts
@@ -1,137 +1,119 @@
-import { Component, OnInit } from '@angular/core';
-import {ConfigService} from '../config.service'
-import { Blog } from '../blog';
-import { BlogService } from '../blog.service';
-import { config } from '../config';
-import { environment } from '../../environments/environment';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ConfigService } from "../config.service";
+import { Blog } from "../blog";
+import { BlogService } from "../blog.service";
+import { config } from "../config";
+import { environment } from "../../environments/environment";
+import { ActivatedRoute, Router } from "@angular/router";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { AlertController } from '@ionic/angular';
-import { Location } from '@angular/common';
+import { AlertController } from "@ionic/angular";
+import { Location } from "@angular/common";
 @Component({
-  selector: 'app-navigationbar',
-  templateUrl: './navigationbar.component.html',
-  styleUrls: ['./navigationbar.component.scss'],
+  selector: "app-navigationbar",
+  templateUrl: "./navigationbar.component.html",
+  styleUrls: ["./navigationbar.component.scss"],
 })
 export class NavigationbarComponent implements OnInit {
-  crossSign:boolean = false;
-  humBurger:boolean = true;
-  mainMenu:boolean =false;
-  mainMenuItem :boolean = true;
+  crossSign: boolean = false;
+  humBurger: boolean = true;
+  mainMenu: boolean = false;
+  mainMenuItem: boolean = true;
   blogs: Blog[];
   config: config;
   public id: string = null;
   private sub: any;
   post: Object;
-  logggenIn:boolean =false
+  logggenIn: boolean = false;
   username: any;
   password: any;
   user: string;
   pass: string;
-  isLoading:boolean=false;
-  logoutUrl="http://gowebtutorial.com/api/json/user/logout"
+  isLoading: boolean = false;
+  logoutUrl = "http://gowebtutorial.com/api/json/user/logout";
   url = "http://latdating.dd:8083/api/json/system/connect";
-  headerDict:any;
+  headerDict: any;
   itrs: any;
-  constructor(private Configservice: ConfigService,
+  constructor(
+    private Configservice: ConfigService,
     private blogService: BlogService,
-              private route: ActivatedRoute,
-              private router: Router,
-              private http: HttpClient,
-              public _location: Location,
-              public alertController: AlertController) { }
- 
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient,
+    public _location: Location,
+    public alertController: AlertController
+  ) {}
+
   ngOnInit() {
-
-    this.itrs=JSON.parse(localStorage.getItem("currentUser"));
-      
-    console.log("Nav" + " "+ this.itrs.token)
-
+    // this.itrs = JSON.parse(localStorage.getItem("currentUser"));
+    // console.log("Nav" + " " + this.itrs.token);
   }
-
-
-
 
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
 
-  humburgereffect()
-  {
+  humburgereffect() {
     this.correctAlert();
   }
-  humBurgerCross()
-  {
+  humBurgerCross() {
     this.crossSign = false;
     this.humBurger = true;
-    this.mainMenu =false;
+    this.mainMenu = false;
   }
-  itemclick()
-  {
-    this.mainMenu = false; 
+  itemclick() {
+    this.mainMenu = false;
     this.crossSign = false;
     this.humBurger = true;
   }
 
-  LoginOpen()
-  {
-
-    if(this.logggenIn==false)
-    {
-  this.logggenIn=true
-    }
-    else if( this.logggenIn==true)
-    {
-      this.logggenIn=false
+  LoginOpen() {
+    if (this.logggenIn == false) {
+      this.logggenIn = true;
+    } else if (this.logggenIn == true) {
+      this.logggenIn = false;
     }
   }
 
+  LoginForm(user, pass) {
+    console.log(user, pass);
+    this.http
+      .post<any>("http://gowebtutorial.com/api/json/user/login", {
+        username: user,
+        password: pass,
+      })
+      .subscribe((data) => {
+        this.post = data.token;
+        this.headerDict = { "X-CSRF-Token": data.token };
+      });
+    return (
+      this.http.post(this.url, { headers: new HttpHeaders(this.headerDict) }),
+      (this.logggenIn = false)
+    );
+  }
 
-  LoginForm(user,pass)
-  {
-    console.log(user,pass)
-    this.http.post<any>('http://gowebtutorial.com/api/json/user/login',{username: user,password:pass}).subscribe(data => {
-            this.post = data.token;
-            this.headerDict ={"X-CSRF-Token":data.token};
-           });
-           return this.http.post(this.url,{headers: new HttpHeaders(this.headerDict)}),
-           this.logggenIn=false
-}
+  close() {
+    this.isLoading = false;
+  }
+  refresh() {
+    this.router.navigate(["/"]);
+  }
+  async correctAlert() {
+    const correct = await this.alertController.create({
+      message: "Logged In",
+      buttons: ["OK"],
+    });
 
-close()
-{
-  this.isLoading=false
-}
-refresh() {
-  
- 
-    this.router.navigate(['/']);
+    await correct.present();
+  }
 
-  
-  
-}
-async correctAlert() {
-  const correct = await this.alertController.create({
-    message: 'Logged In',
-    buttons: ['OK']
-  });
+  logOut() {
+    let headers = new HttpHeaders();
+    console.log(this.itrs.token);
 
-  await correct.present();
-}
-
-
-logOut()
-{
-  let headers = new HttpHeaders();
- console.log(this.itrs.token)
-
- headers = headers.set('X-CSRF-Token',  this.itrs.token);
-console.log({headers: headers})
- this.http.post(this.logoutUrl,  {headers: headers}).subscribe((head)=>{
-   console.log(head)
- });
-
-
-
-}
+    headers = headers.set("X-CSRF-Token", this.itrs.token);
+    console.log({ headers: headers });
+    this.http.post(this.logoutUrl, { headers: headers }).subscribe((head) => {
+      console.log(head);
+    });
+  }
 }
--- app/signin/signin.page.ts
+++ app/signin/signin.page.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { MatTabChangeEvent } from "@angular/material/tabs";
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router } from "@angular/router";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { AlertController } from '@ionic/angular';
+import { AlertController } from "@ionic/angular";
+
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.page.html',
-  styleUrls: ['./signin.page.scss'],
+  selector: "app-signin",
+  templateUrl: "./signin.page.html",
+  styleUrls: ["./signin.page.scss"],
 })
 export class SigninPage implements OnInit {
   maxNumberOfTabs = 5;
@@ -18,19 +19,21 @@ export class SigninPage implements OnInit {
   pass: any;
   password: any;
   currentUserSubject: any;
-  newdata:any;
+  newdata: any;
   datas: any;
-  itr:any;
+  itr: any;
   data: any;
   itrs: any;
-  token:any;
-  constructor( private router: Router,
-    private http: HttpClient,public alertController: AlertController) { }
+  token: any;
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    public alertController: AlertController
+  ) {}
 
   ngOnInit() {
-    this.itrs=JSON.parse(localStorage.getItem("currentUser"));
-      
-    console.log(this.itrs.token)
+    // this.itrs = JSON.parse(localStorage.getItem("currentUser"));
+    // console.log(this.itrs.token);
   }
   public tabChanged(tabChangeEvent: MatTabChangeEvent): void {
     this.selectedIndex = tabChangeEvent.index;
@@ -43,47 +46,47 @@ export class SigninPage implements OnInit {
     this.selectedIndex -= 1;
   }
   public nextSteps() {
-    this.LoginForm(this.user,this.password)
+    this.LoginForm(this.user, this.password);
     this.selectedIndex += 1;
   }
-  
 
-  LoginForm(user,pass)
-  {
+  LoginForm(user, pass) {
+    this.http
+      .post<any>("https://gowebtutorial.com/api/json/user/login", {
+        username: user,
+        password: pass,
+      })
+      .subscribe((data) => {
+        this.post = data;
+        localStorage.setItem("currentUser", JSON.stringify(data));
+        this.itr = JSON.parse(localStorage.getItem("currentUser"));
+        console.log(this.itr);
 
+        const headers = new HttpHeaders()
+          .set("X-CSRF-Token", this.itr.token)
+          .set("Content-Type", "application/json")
+          .set("X-Cookie", this.itr.session_name + "=" + this.itr.sessid);
 
+        const requestOptions = {
+          headers: headers,
+          withCredentials: true,
+        };
 
+        this.http.post<any>(this.url, {}, requestOptions).subscribe((head) => {
+          console.log(
+            "This is the response after sending token to connect " +
+              JSON.stringify(head)
+          );
+        });
+      });
+  }
 
+  async correctAlert() {
+    const correct = await this.alertController.create({
+      message: "Logged In",
+      buttons: ["OK"],
+    });
 
-
-    console.log(user,pass)
-    this.http.post<any>('https://gowebtutorial.com/api/json/user/login',{username:user,password:pass}).subscribe(data => {
-            this.post = data;
-            console.log(data)
-            localStorage.setItem('currentUser', JSON.stringify(data));
-
-            this.itr=JSON.parse(localStorage.getItem("currentUser"));
-            console.log(this.itr.token)
-
-         
-            let headers = new HttpHeaders();
-            
-            headers = headers.set('X-CSRF-Token',  this.itr.token);
-           console.log({headers: headers})
-            this.http.post(this.url,  {headers: headers}).subscribe((head)=>{
-              console.log(head)
-            });
-       
-
-           });
-           
-          }   
-          async correctAlert() {
-            const correct = await this.alertController.create({
-              message: 'Logged In',
-              buttons: ['OK']
-            });
-        
-            await correct.present();
-          }
+    await correct.present();
+  }
 }
